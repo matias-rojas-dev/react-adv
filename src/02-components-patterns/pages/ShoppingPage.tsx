ff3fb63b840e10acca33e22f5a9f31010a9e612b
matias-rojas-dev <maigrojas.dev@gmail.com>
2024-09-14 10:13:09 -0300

    Components patterns

---
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,39 @@
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from '../components/ProductCard'
+
+const product = {
+  id: '1',
+  title: 'Coffee Mug - 2',
+  img: './coffee-mug.png',
+}
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>ShoppingPage</h1>
+      <hr />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        <ProductCard product={product}>
+          <ProductCard.Image img={product.img} />
+          <ProductCard.Title title="Hola mundo" />
+          <ProductCard.Buttons />
+        </ProductCard>
+        <ProductCard product={product}>
+          <ProductImage img={product.img} />
+          <ProductTitle />
+          <ProductButtons />
+        </ProductCard>
+      </div>
+    </div>
+  )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,26 +2,33 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
-} from 'react-router-dom';
+  NavLink,
+} from 'react-router-dom'
 
-import logo from '../logo.svg';
+import logo from '../logo.svg'
+import { ShoppingPage } from '../02-components-patterns/pages/ShoppingPage'
 
 export const Navigation = () => {
   return (
     <Router>
       <div className="main-layout">
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/" activeClassName="nav-active" exact>
+                Shopping
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" activeClassName="nav-active" exact>
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" activeClassName="nav-active" exact>
+                Users
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -29,8 +36,8 @@ export const Navigation = () => {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <h1>About</h1>
+          <Route path="/">
+            <ShoppingPage />
           </Route>
           <Route path="/users">
             <h1>Users</h1>
@@ -41,5 +48,5 @@ export const Navigation = () => {
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
